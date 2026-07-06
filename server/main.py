4308088c7841b06c3c73a49c97f3c45aac7fd35c
Praveen Manasa <it.intern@theviswagroup.com>
2026-07-06 14:00:47 +0800

    Updated CORS for production

--- server/main.py
+++ server/main.py
@@ -31,7 +31,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://vams.theviswagroup.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
